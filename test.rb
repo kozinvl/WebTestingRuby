a7b985b932378ea104e565ee47e031294e3117d2
vkozin <vladiksystem@gmail.com>
2018-07-20 14:35:16 +0300

    refactoring

--- test.rb
+++ test.rb
@@ -11,7 +11,6 @@ class TestCommentsPage < Test::Unit::TestCase
     @main_page = MainPage.new @driver
   end
 
-
   def test_title_page
     @actual_title = @main_page.get_title_page
     expected_title = 'Index'
@@ -20,32 +19,32 @@ class TestCommentsPage < Test::Unit::TestCase
 
   def test_new_comment
     actually_comment = @main_page
-                       .new_comment_button
-                       .filling_text('New Comment Row')
-                       .chose_one_category
-                       .save_condition
-                       .return
-                       .change_status('All')
-                       .apply
-                       .sort_by_number
-                       .get_first_column
+                           .new_comment_button
+                           .filling_text('New Comment Row')
+                           .chose_one_category
+                           .save_condition
+                           .return
+                           .change_status('All')
+                           .apply
+                           .sort_by_number
+                           .get_first_column
     assert_equal('New Comment Row', actually_comment)
   end
 
   def test_delete_comment
     actually_comment = @main_page
-                       .select_choose_box(2)
-                       .edit_button
-                       .filling_text('ABC comment', true)
-                       .checkbox_status
-                       .chose_one_category
-                       .save_condition
-                       .return
-                       .change_status('Active')
-                       .apply
-                       .sort_by_number
-                       .refresh_by_comment_text
-                       .get_first_column
+                           .select_choose_box(2)
+                           .edit_button
+                           .filling_text('ABC comment', true)
+                           .checkbox_status
+                           .chose_one_category
+                           .save_condition
+                           .return
+                           .change_status('Active')
+                           .apply
+                           .sort_by_number
+                           .refresh_by_comment_text
+                           .get_first_column
     assert_equal('ABC comment', actually_comment)
   end
 
